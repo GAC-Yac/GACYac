9500d2cebcedfaad84ae4d08a60f463030de9b0c
luisarroyo <larroyo@gustavus.edu>
2022-04-07 13:42:34 -0500

    added button to title, along with the implementation to change title within that button title.

--- app/src/main/java/com/example/gacyac/MainActivity.kt
+++ app/src/main/java/com/example/gacyac/MainActivity.kt
@@ -4,6 +4,8 @@ import android.content.ContentValues.TAG
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Button
+import android.widget.EditText
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.ValueEventListener
@@ -12,9 +14,12 @@ import com.google.firebase.database.ktx.getValue
 import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var titleButton: Button
+    private lateinit var editTitle: EditText
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        titleButton = findViewById(R.id.post_title)
 
         val database = Firebase.database
         val myRef = database.getReference("message")
@@ -35,8 +40,16 @@ class MainActivity : AppCompatActivity() {
                 Log.w(TAG, "Failed to read value.", error.toException())
             }
         })
+        fun changeTitle(titleButton: Button){
+            val editTextValue: String = titleButton.getText().toString()
+            editTitle.setText(editTextValue)
+        }
 
+        titleButton.setOnClickListener {
+            //clicking title opens up activity for the post
+        }
 
     }
 
 }
+
