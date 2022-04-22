4c7314be7b7aa73c560cc4c420f1ef5d0ba54648
NikitaSandstrom <NikitaSandstrom1@gmail.com>
2022-04-21 19:51:42 -0500

    Master Branch fully updated and done right

--- app/src/main/java/com/example/gacyac/MainActivity.kt
+++ app/src/main/java/com/example/gacyac/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.gacyac
 
+import android.content.ContentValues
 import android.content.ContentValues.TAG
 import android.media.metrics.Event
 import androidx.appcompat.app.AppCompatActivity
@@ -48,136 +49,6 @@ class MainActivity : AppCompatActivity() {
             adapter = PostAdapter(postList)
         }
 
-        // Displaying Posts
-        //fillWithPosts()
-        val mainActivity = this
-
-        // Button Functionality
-        var addButton: Button = findViewById(R.id.btnAddPost)
-        addButton.setOnClickListener {
-            val intent = CreatePost.newIntent(this)
-            startActivity(intent)
-        }
-       // userDetails.setOnClickListener{
-           // val intent =UserProfile.newIntent(this)
-            //startActivity(intent)
-
-      //  }
-
-        var bpButton: Button = findViewById(R.id.btnBonusPoints)
-
-        bpButton.setOnClickListener{
-            val intent = UserProfile.newIntent(this)
-            startActivity(intent)
-           // postList.clear()
-        }
-
-        EventChangeListener()
-
-    }
-
-    private fun EventChangeListener() {
-        database = FirebaseFirestore.getInstance()
-        database.collection("newPosts").orderBy("time").
-            addSnapshotListener(object : EventListener<QuerySnapshot> {
-                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
-                    if (error != null) {
-                        Log.e("ERROR", error.message.toString())
-                        return
-                    }
-
-                    for (dc :DocumentChange in value?.documentChanges!!) {
-                        if (dc.type == DocumentChange.Type.ADDED) {
-                            postList.add(dc.document.toObject(Post::class.java))
-
-                        }
-                    }
-                }
-
-            })
-    }
-
-    override fun onStart() {
-        super.onStart()
-    }
-
-
-
-
-
-
-
-
-    /*private fun fillWithPosts() {
-        val database = Firebase.firestore
-        val docRef = database.collection("posts")
-        /*
-        for(){
-            // for each post instance in the database, make a Post object and fill it with the contents from each database post
-            val post = Post()
-        }*/
-
-        val testPost = Post(
-            "Test Post",
-            "This is a test post.",
-            0,
-            "Anonymous",
-            Date(System.currentTimeMillis()),
-            "0"
-        )
-
-        postList.add(testPost)
-    }*/
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//setContentView(R.layout.activity_main)
-//titleButton = findViewById(R.id.post_title)
-
-        val database = Firebase.firestore
-
         // randomly creates a username
         fun createRandomUsername(): String{
             val colors = resources.openRawResource(R.raw.colors).bufferedReader().readLines()
@@ -194,7 +65,7 @@ class MainActivity : AppCompatActivity() {
             database.collection("users")
                 .document(device_id).set(user)
                 .addOnSuccessListener { documentReference ->
-                    Log.d(TAG, "document added with username $username")
+                    Log.d(ContentValues.TAG, "document added with username $username")
                     var newUserToast = Toast.makeText(this, "Welcome New User!", Toast.LENGTH_SHORT)
                     var newUsernameToast = Toast.makeText(this, "Your random (anonymous) username Is: $username", Toast.LENGTH_LONG)
                     newUserToast.setGravity(Gravity.TOP, 0, 200)
@@ -203,7 +74,7 @@ class MainActivity : AppCompatActivity() {
                     newUsernameToast.show()
                 }
                 .addOnFailureListener { e ->
-                    Log.w(TAG, "Error adding document", e)
+                    Log.w(ContentValues.TAG, "Error adding document", e)
                 }
             return username;
         }
@@ -212,21 +83,21 @@ class MainActivity : AppCompatActivity() {
         fun attemptLogin(device_id: String){
             database.collection("users").document(device_id).get()
                 .addOnSuccessListener { documentReference ->
-                    Log.d(TAG, "data has been retrieved")
+                    Log.d(ContentValues.TAG, "data has been retrieved")
                     if (documentReference!!.get("username") == null){
                         username = createNewUser(device_id)
-                        Log.d(TAG, "new user created")
+                        Log.d(ContentValues.TAG, "new user created")
                     }
                     else {
                         username = documentReference!!.get("username").toString()
-                        Log.d(TAG, "user already exists, username is $username")
+                        Log.d(ContentValues.TAG, "user already exists, username is $username")
                         var loginToast = Toast.makeText(this, "Welcome back, $username", Toast.LENGTH_LONG)
                         loginToast.setGravity(Gravity.TOP, 0, 200)
                         loginToast.show()
                     }
                 }
                 .addOnFailureListener { e ->
-                    Log.w(TAG, "Could not Log In", e)
+                    Log.w(ContentValues.TAG, "Could not Log In", e)
                 }
         }
         //fun changeTitle(titleButton: Button){
@@ -259,18 +130,18 @@ class MainActivity : AppCompatActivity() {
             val intent = CreatePost.newIntent(this)
             startActivity(intent)
         }
-       // userDetails.setOnClickListener{
-           // val intent =UserProfile.newIntent(this)
-            //startActivity(intent)
+        // userDetails.setOnClickListener{
+        // val intent =UserProfile.newIntent(this)
+        //startActivity(intent)
 
-      //  }
+        //  }
 
         var bpButton: Button = findViewById(R.id.btnBonusPoints)
 
         bpButton.setOnClickListener{
             val intent = UserProfile.newIntent(this)
             startActivity(intent)
-           // postList.clear()
+            // postList.clear()
         }
 
         EventChangeListener()
@@ -280,22 +151,22 @@ class MainActivity : AppCompatActivity() {
     private fun EventChangeListener() {
         database = FirebaseFirestore.getInstance()
         database.collection("newPosts").orderBy("time").
-            addSnapshotListener(object : EventListener<QuerySnapshot> {
-                override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
-                    if (error != null) {
-                        Log.e("ERROR", error.message.toString())
-                        return
-                    }
-
-                    for (dc :DocumentChange in value?.documentChanges!!) {
-                        if (dc.type == DocumentChange.Type.ADDED) {
-                            postList.add(dc.document.toObject(Post::class.java))
-
-                        }
-                    }
+        addSnapshotListener(object : EventListener<QuerySnapshot> {
+            override fun onEvent(value: QuerySnapshot?, error: FirebaseFirestoreException?) {
+                if (error != null) {
+                    Log.e("ERROR", error.message.toString())
+                    return
                 }
 
-            })
+                for (dc :DocumentChange in value?.documentChanges!!) {
+                    if (dc.type == DocumentChange.Type.ADDED) {
+                        postList.add(dc.document.toObject(Post::class.java))
+
+                    }
+                }
+            }
+
+        })
     }
 
     override fun onStart() {
@@ -331,58 +202,3 @@ class MainActivity : AppCompatActivity() {
     }*/
 
 }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//setContentView(R.layout.activity_main)
-//titleButton = findViewById(R.id.post_title)
-
-
-//fun changeTitle(titleButton: Button){
-//    val editTextValue: String = titleButton.getText().toString()
-//    editTitle.setText(editTextValue)
-//}
-
-//titleButton.setOnClickListener {
-//clicking title opens up activity for the post
-//}
