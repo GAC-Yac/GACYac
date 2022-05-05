62d5c4f582b04f2918ac74e4df2c558683d29ed8
lukeblazing <lukeblazing@gmail.com>
2022-05-05 12:27:18 -0500

    button for leaderboard in the user home toolbar

--- app/src/main/java/com/example/gacyac/MainActivity.kt
+++ app/src/main/java/com/example/gacyac/MainActivity.kt
@@ -1,20 +1,18 @@
 package com.example.gacyac
 
+import android.R
 import android.content.ContentValues
-import android.content.Context
-import android.content.Intent
 import android.os.Bundle
 import android.provider.Settings.Secure
 import android.util.Log
 import android.view.Gravity
 import android.view.MenuItem
-import android.view.View
 import android.widget.ImageButton
-import android.widget.TextView
 import android.widget.Toast
-import androidx.appcompat.app.ActionBarDrawerToggle
+import androidx.appcompat.app.ActionBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
 import androidx.recyclerview.widget.GridLayoutManager
 import com.example.gacyac.databinding.ActivityMainBinding
@@ -24,34 +22,42 @@ import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.sql.Date
 
+
 @Suppress("DEPRECATION")
-class MainActivity : AppCompatActivity()  {
+class MainActivity : AppCompatActivity() {
     private lateinit var androidID: String
     private lateinit var username: String
+    private lateinit var toolbar : Toolbar
+
     private lateinit var binding: ActivityMainBinding
     private var database = Firebase.firestore
 
-    private  var toolbar : Toolbar? = null
-    private var mDrawerLayout: DrawerLayout? = null
+    private lateinit var mDrawerLayout: DrawerLayout
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-
-
-
-
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-       toolbar = findViewById<Toolbar>(R.id.homeToolbar)
+        toolbar = findViewById(R.id.homeToolbar)
         setSupportActionBar(toolbar)
 
-       initNavigationDrawer()
+        val actionbar: ActionBar? = supportActionBar
+            actionbar?.apply {
+                setDisplayHomeAsUpEnabled(true)
+                setHomeAsUpIndicator(R.drawable.ic_baseline_menu_24)
+            }
+
+        mDrawerLayout = findViewById(R.id.drawerLayout)
+
+        val  navigationView: NavigationView = findViewById(R.id.nav_view)
+        navigationView.setNavigationItemSelectedListener { menuItem ->
+            menuItem.isChecked = true
+            true
+        }
+
         // randomly creates a username
-        fun createRandomUsername(): String {
+        fun createRandomUsername(): String{
             val colors = resources.openRawResource(R.raw.colors).bufferedReader().readLines()
             val nouns = resources.openRawResource(R.raw.nouns).bufferedReader().readLines()
             return colors.random().toUpperCase() + " " + nouns.random().toUpperCase()
@@ -70,11 +76,7 @@ class MainActivity : AppCompatActivity()  {
                 .addOnSuccessListener {
                     Log.d(ContentValues.TAG, "document added with username $username")
                     val newUserToast = Toast.makeText(this, "Welcome New User!", Toast.LENGTH_SHORT)
-                    val newUsernameToast = Toast.makeText(
-                        this,
-                        "Your random (anonymous) username Is: $username",
-                        Toast.LENGTH_LONG
-                    )
+                    val newUsernameToast = Toast.makeText(this, "Your random (anonymous) username Is: $username", Toast.LENGTH_LONG)
                     newUserToast.setGravity(Gravity.TOP, 0, 200)
                     newUsernameToast.setGravity(Gravity.TOP, 0, 200)
                     newUserToast.show()
@@ -87,18 +89,18 @@ class MainActivity : AppCompatActivity()  {
         }
 
         // should be called on every app start, either logs in user or creates new user
-        fun attemptLogin(device_id: String) {
+        fun attemptLogin(device_id: String){
             database.collection("users").document(device_id).get()
                 .addOnSuccessListener { documentReference ->
                     Log.d(ContentValues.TAG, "data has been retrieved")
-                    if (documentReference!!.get("username") == null) {
+                    if (documentReference!!.get("username") == null){
                         username = createNewUser(device_id)
                         Log.d(ContentValues.TAG, "new user created")
-                    } else {
+                    }
+                    else {
                         username = documentReference.get("username").toString()
                         Log.d(ContentValues.TAG, "user already exists, username is $username")
-                        val loginToast =
-                            Toast.makeText(this, "Welcome back, $username", Toast.LENGTH_LONG)
+                        val loginToast = Toast.makeText(this, "Welcome back, $username", Toast.LENGTH_LONG)
                         loginToast.setGravity(Gravity.TOP, 0, 200)
                         loginToast.show()
                     }
@@ -110,21 +112,19 @@ class MainActivity : AppCompatActivity()  {
 
 
         // retrieve unique device identifier
-        androidID = Secure.getString(
-            getApplicationContext().getContentResolver(),
-            Secure.ANDROID_ID
-        )
+        androidID = Secure.getString(getApplicationContext().getContentResolver(),
+            Secure.ANDROID_ID)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        binding.postRecyclerView.apply {
+        binding.postRecyclerView.apply{
             layoutManager = GridLayoutManager(applicationContext, 1)
             adapter = PostAdapter(postList)
         }
 
 
         // start the log in process with the unique device identifier
-        attemptLogin(androidID)
+        attemptLogin(androidID+7)
 
 
         // button to create a new post
@@ -137,37 +137,17 @@ class MainActivity : AppCompatActivity()  {
         }
 
         val bpButton: ImageButton = findViewById(R.id.btnBonusPoints)
-        bpButton.setOnClickListener {
+        bpButton.setOnClickListener{
             val intent = UserProfile.newIntent(this)
             startActivity(intent)
         }
-        val navigationView: NavigationView = findViewById(R.id.nav_view)
-        val headerView : View = navigationView.getHeaderView(0)
-        val navUsername : TextView = headerView.findViewById(R.id.navigation_header_username)
-        database.collection("users").document(androidID).get()
-            .addOnSuccessListener { documentReference ->
-                val grabUserName = documentReference.get("username").toString()
-                navUsername.text = grabUserName
-            }
+
+        val leaderboardButton: ImageButton = findViewById(R.id.btnLeaderboard)
+        leaderboardButton.setOnClickListener{
+            print("start the leaderboard fragment")
+        }
 
         eventChangeListener()
-    }
-
-
-    private fun initNavigationDrawer() {
-
-        val navigationView = findViewById<NavigationView>(R.id.nav_view)
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            true
-        }
-        val header = navigationView.getHeaderView(0)
-        mDrawerLayout = findViewById<DrawerLayout>(R.id.drawerLayout)
-
-        val actionBarDrawerToggle = object : ActionBarDrawerToggle(this, mDrawerLayout, toolbar, R.string.open, R.string.close) {
-        }
-
-        mDrawerLayout!!.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
 
     }
 
@@ -192,14 +172,18 @@ class MainActivity : AppCompatActivity()  {
 
         })
     }
-    companion object {
 
-        fun newIntent(context: Context): Intent {
-            return Intent(context, MainActivity::class.java)
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            android.R.id.home -> {
+                mDrawerLayout.openDrawer(GravityCompat.START)
+                true
+            }
+
+            else -> super.onOptionsItemSelected(item)
         }
     }
-
-
 
 
 
