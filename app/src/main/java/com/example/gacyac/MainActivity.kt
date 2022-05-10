3abeebf209562899a1d72c854964d6e5354f6b0c
NikitaSandstrom <NikitaSandstrom1@gmail.com>
2022-05-09 20:20:44 -0500

    Small Changes

--- app/src/main/java/com/example/gacyac/MainActivity.kt
+++ app/src/main/java/com/example/gacyac/MainActivity.kt
@@ -330,7 +330,7 @@ class MainActivity : AppCompatActivity()  {
 
         database.collection("newererPosts").orderBy("postID", Query.Direction.DESCENDING).limit(1).get().addOnSuccessListener { documents ->
             currentPostCount = headerView.findViewById(R.id.navigation_header_current_post_count)
-            currentPostCount.setText("Current Post Count: ")
+            currentPostCount.setText("Current Post Count: ${documents.documents.get(0).get("postID")}")
             //${documents.documents.get(0).get("postID")}
         }
 
